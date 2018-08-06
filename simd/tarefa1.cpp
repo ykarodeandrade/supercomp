07f672db2d3e0a77a3a6b28f385fabdc1256621c
Igor <igordsm@gmail.com>
2018-08-06 07:01:03 -0300

    Arquivos aula 01

--- simd/tarefa1.cpp
+++ simd/tarefa1.cpp
@@ -1,8 +1,8 @@
 // Original: Nicolas Brailovsky
 
-#define SIZE (5)
+#define SIZE (400)
 long sum(int v[SIZE]) throw() {
-    long s = 0;
+    int s = 0; // este exemplo eh didatico. soma de ints deveria ser long ;)
     for (unsigned i=0; i<SIZE; i++) s += v[i];
     return s;
 }
