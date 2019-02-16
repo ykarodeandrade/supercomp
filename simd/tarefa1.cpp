a71b592ed2e8c85a5cab50a31afedb7a32dcdf99
Luciano Soares <lpsoares@Lucianos-MacBook-Pro.local>
2019-02-16 15:41:15 -0200

    Iniciando a versão 2019-1 do curso

--- simd/tarefa1.cpp
+++
@@ -1,8 +0,0 @@
-// Original: Nicolas Brailovsky
-
-#define SIZE (400)
-long sum(int v[SIZE]) throw() {
-    int s = 0; // este exemplo eh didatico. soma de ints deveria ser long ;)
-    for (unsigned i=0; i<SIZE; i++) s += v[i];
-    return s;
-}
