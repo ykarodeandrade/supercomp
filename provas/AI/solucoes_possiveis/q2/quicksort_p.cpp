1dac1f8f722043d290f30f64cf48283f774ad83d
Luciano Soares <lpsoares@gmail.com>
2019-04-29 08:53:02 -0300

    Soluções Possíveis para Prova

---
+++ provas/AI/solucoes_possiveis/q2/quicksort_p.cpp
@@ -0,0 +1,115 @@
+// Questão 2:
+// O seguinte código de quicksort funciona perfeitamente, contudo queremos mais
+// Paralelize esse código de alguma forma eficiente usando um ambiente de memória compartilhada (OpenMP)
+// Dica: se a quantidade de dados for muito pequena começa a não ser interessante paralelizar
+
+// Tempo original = 1.00s
+// Tempo paralelizado = 0.25s
+
+
+#include <iostream>
+#include <boost/random.hpp>
+#include <omp.h>
+
+#define SIZE  10000000
+#define DELTA 10000
+
+// escolhe um pivot, posiciona ele e divide o vetor em duas partes
+unsigned int partition(std::vector<int>& vec, unsigned int low, unsigned int high) {
+    
+    int pivot = vec[high];          // escolhe um pivot 
+    unsigned int index = (low - 1); // indice dos elementos de busca - 1
+
+    for (unsigned int j = low; j<=high-1; j++) { 
+        if (vec[j] <= pivot) {
+            index++;
+            std::swap(vec[index], vec[j]); 
+        } 
+    } 
+    std::swap(vec[index+1], vec[high]); 
+    return(index+1);
+}
+
+// Ordena os dados no vetor de low até high
+void quicksort(std::vector<int>& vec, unsigned int low, unsigned int high)  { 
+
+    if (low < high) {
+
+        unsigned int pi = partition(vec, low, high); // particiona o vetor
+  
+        if(pi-low>DELTA) {  // checa se o tamanho justifica para paralelizar
+            #pragma omp task default(none) shared(vec) firstprivate(low,pi)
+            quicksort(vec, low, pi-1); 
+        } else {
+            quicksort(vec, low, pi-1);
+        }
+
+        if(high-pi>DELTA) {  // checa se o tamanho justifica para paralelizar
+            #pragma omp task default(none) shared(vec) firstprivate(high,pi)
+            quicksort(vec, pi+1, high);
+        } else {
+            quicksort(vec, pi+1, high);
+        }
+            
+        #pragma omp taskwait
+    } 
+} 
+  
+// Mostra parte dos elementos do vetor
+void printArray(std::vector<int>& vec) {
+
+    unsigned int size = vec.size();
+    unsigned int i;
+    if(size<10) {
+        for (i=0; i < size; i++) 
+            std::cout << "#" << i << " : " << vec[i] << std::endl; 
+    } else {
+        for (i=0; i < 10; i++) {
+            std::cout << "#" << i << " : " << vec[i] << std::endl;
+        }
+        if(size<20) {
+            for (i=10; i < size; i++) 
+                std::cout << "#" << i << " : " << vec[i] << std::endl; 
+        } else {
+            std::cout << "..." << std::endl; 
+            for (i=size-10; i < size; i++) {
+                std::cout << "#" << i << " : " << vec[i] << std::endl; 
+            }
+        }
+    }
+
+} 
+  
+
+int main() {
+
+    std::vector<int> vec;
+
+    // Gera uma série de número aleatórios
+    boost::random::mt19937 gen{27};
+    for(unsigned int i=0;i<SIZE;i++) {
+        vec.push_back(gen());
+    }
+
+    double start_time, run_time;
+    start_time = omp_get_wtime();
+    
+    #pragma omp parallel
+    {
+        #pragma omp single nowait
+        {
+            quicksort(vec, 0, SIZE-1);  // processa o quicksort
+        }
+    }
+
+    run_time = omp_get_wtime() - start_time;
+    std::cout << "Tempo decorrido do quicksort: " << run_time << "s" << std::endl;
+
+    std::cout << "Valores obtidos : " << std::endl;
+    printArray(vec);
+
+    return 0; 
+} 
+
+
+
