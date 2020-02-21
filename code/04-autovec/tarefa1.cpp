20541c7bd8b77ca24837246d6880f4f0968bc19c
Igor Montagner <igordsm@gmail.com>
2020-02-21 07:58:59 -0300

    Exemplo de autovetorização consertado

--- code/04-autovec/tarefa1.cpp
+++ code/04-autovec/tarefa1.cpp
@@ -2,8 +2,7 @@
 
 #define SIZE (400)
 long sum(long v[SIZE]) {
-    long *d = new long[SIZE];
-    long s = 0; // este exemplo eh didatico. soma de ints deveria ser long ;)
+    long s = 0;
     for (long i=0; i<SIZE; i++) s += v[i];
     return s;
 }
