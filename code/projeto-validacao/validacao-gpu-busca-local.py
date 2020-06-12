8df2dd8e1280f35de75f0d8262244634b5b9c2ed
Igor Montagner <igordsm@gmail.com>
2020-06-12 14:37:34 -0300

    Validação GPU com tempos limite mais justos.

---
+++ code/projeto-validacao/validacao-gpu-busca-local.py
@@ -0,0 +1,24 @@
+from grading_utils import PerformanceTest, TestConfiguration, RepeaterTest
+import sys
+from validacao_pfe import BuscaLocalTest, TestePFERepeticaoParalela
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Uso: ./validacao-gpu-busca-local executavel-gpu')
+        sys.exit(-1)
+
+    tests = TestConfiguration.from_pattern('entradas', 'in_heur', 
+        check_stderr=False, environ={'ITER': '10000'}, time_limit=1.0)
+
+    teste_grande = TestConfiguration.from_file('entradas/in_local_72_24_5', 'entradas/out_local_72_24_5', 
+        check_stderr=False, environ={'ITER': '10000'}, time_limit=2.5)
+    tests['Grande'] = teste_grande
+
+    t = BuscaLocalTest(sys.argv[1], tests)
+    t.main()
+
+    teste_repetido = TestConfiguration.from_file('entradas/in_local_72_24_5', 'entradas/out_local_72_24_5', check_stderr=False,
+                                    environ={'ITER': '10', 'SEED': '10'})
+    r = TestePFERepeticaoParalela(sys.argv[1], {'Execucao %d'%i :teste_repetido for i in range(10)})
+    r.main()
+
